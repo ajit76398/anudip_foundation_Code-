81e018bc2d17c95529e9f43defaeacf1857b06c7
I-net computer <I-net computer@DESKTOP-IS5L1H2>
2022-12-02 20:11:43 +0530

    version-1

---
+++ 1_Dec_2022PracticalTest/src/GameService.java
@@ -0,0 +1,9 @@
+
+public class GameService {
+
+	
+	void getcitynameservice()
+	{
+		
+	}
+}
---
+++ 1_Dec_2022PracticalTest/src/com/mygame/dao/GameDao.java
@@ -0,0 +1,18 @@
+package com.mygame.dao ;
+public class GameDao {
+	
+
+	
+	
+	  public void getcitynames()
+	{
+		  String c[]={"Mumbai","Chennai","Dehli","Kolkata","hyderabad"};
+       	
+	}
+	 
+	
+	public static void main(String ar[])
+	{
+		getcitynames();
+	}
+}
